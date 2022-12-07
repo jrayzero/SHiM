b4fe7d6f7274169c604bb036d512bd63fa56da1f
Jessica Ray <jray@csail.mit.edu>
2022-12-07 00:45:25 -0500

    okay, now actually fix the syntax printout for Elem[]

--- hmda/staged/staged.h
+++ hmda/staged/staged.h
@@ -7,7 +7,16 @@ namespace hmda {
 // fixes the syntax for staged function args of the form
 // dyn_var<T[]>
 // buildit prints "T[] arg", but correct syntax is "T arg[]"
-class hmda_c_code_generator : public block::c_code_generator {
+class hmda_cpp_code_generator : public block::c_code_generator {
+public:
+  hmda_cpp_code_generator(std::ostream &_oss) : block::c_code_generator(_oss) { }
+  static void generate_code(block::block::Ptr ast, std::ostream &oss, int indent = 0) {
+    hmda_cpp_code_generator generator(oss);
+    generator.curr_indent = indent;
+    ast->accept(&generator);
+    oss << std::endl;
+  }
+
   void visit(block::func_decl::Ptr a) override {
     a->return_type->accept(this);
     if (a->hasMetadata<std::vector<std::string>>("attributes")) {
@@ -64,7 +73,7 @@ void stage(Func func, std::string name, std::ostream &output, Args...args) {
   auto ast = builder::builder_context().extract_function_ast(func, name, args...);
   block::eliminate_redundant_vars(ast);
   output << "#include \"hmda_runtime.h\"" << std::endl;
-  hmda_c_code_generator::generate_code(ast, output, 0);
+  hmda_cpp_code_generator::generate_code(ast, output, 0);
 }
 
 }
