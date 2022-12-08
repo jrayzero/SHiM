7b54305fef1f518fd68efa9939bc2de90f417b8d
Jessica Ray <jray@csail.mit.edu>
2022-12-07 23:06:51 -0500

    expand the wrapper code

--- hmda/staged/fwrappers.h
+++ hmda/staged/fwrappers.h
@@ -6,113 +6,95 @@
 #include "common/loop_type.h"
 #include "fwddecls.h"
 
-// wrappers for functions that we generate calls to in the generated code
+// These are the wrappers for various functions that we want to generate calls for in the generated code.
 
+// I think these need to be generated outside the staging itself, so I don't really know what a better way
+// to write these is other than manually enumerating them.
 namespace hmda {
 
 builder::dyn_var<void(int)> hexit = builder::as_global("exit");
 builder::dyn_var<loop_type(loop_type)> hfloor = builder::as_global("floor");
 
-// casting functions (type isn't quite right, but don't care!)
-// these are called on dyn_vars. If you want to cast a Ref, call rcast
-#define CAST(dtype)							\
-  builder::dyn_var<dtype(dtype)> cast_##dtype = builder::as_global("hmda::cast<"#dtype ">");
-CAST(uint8_t);
-CAST(uint16_t);
-CAST(uint32_t);
-CAST(uint64_t);
-CAST(int16_t);
-CAST(int32_t);
-CAST(int64_t);
-CAST(float);
-CAST(double);
+// Note: the arg tyoe for these casts isn't right, but I don't want to write every combination
+// of dyn_var<to(from)>, and buildit doesn't seem to care.
+builder::dyn_var<uint8_t(uint8_t)> cast_uint8_t = builder::as_global("hmda::cast<uint8_t>");
+builder::dyn_var<uint16_t(uint16_t)> cast_uint16_t = builder::as_global("hmda::cast<uint16_t>");
+builder::dyn_var<uint32_t(uint32_t)> cast_uint32_t = builder::as_global("hmda::cast<uint32_t>");
+builder::dyn_var<uint64_t(uint64_t)> cast_uint64_t = builder::as_global("hmda::cast<uint64_t>");
+builder::dyn_var<int16_t(int16_t)> cast_int16_t = builder::as_global("hmda::cast<int16_t>");
+builder::dyn_var<int32_t(int32_t)> cast_int32_t = builder::as_global("hmda::cast<int32_t>");
+builder::dyn_var<int64_t(int64_t)> cast_int64_t = builder::as_global("hmda::cast<int64_t>");
+builder::dyn_var<float(float)> cast_float = builder::as_global("hmda::cast<float>");
+builder::dyn_var<double(double)> cast_double = builder::as_global("hmda::cast<double>");
 
-// Things for accessing the underlying data of blocks and views
-#define READER(dtype)							\
-  builder::dyn_var<dtype(dtype*,loop_type)> read_##dtype = builder::as_global("hmda::read<" # dtype ">");
-#define WRITER(dtype)							\
-  builder::dyn_var<void(dtype*,loop_type,dtype)> write_##dtype = builder::as_global("hmda::write<" # dtype ">");
-#define HREADER(dtype)							\
-  builder::dyn_var<dtype(HEAP_T<dtype>,loop_type)> read_heaparr_##dtype = builder::as_global("hmda::read<" # dtype ">");
-#define HWRITER(dtype)							\
-  builder::dyn_var<void(HEAP_T<dtype>,loop_type,dtype)> write_heaparr_##dtype = builder::as_global("hmda::write<" # dtype ">");
-#define MEMSET(dtype)							\
-  builder::dyn_var<void(dtype*,dtype,loop_type)> memset_##dtype = builder::as_global("hmda::hmemset<" # dtype ">");
-#define HMEMSET(dtype)							\
-  builder::dyn_var<void(HEAP_T<dtype>,dtype,loop_type)> memset_heaparr_##dtype = builder::as_global("hmda::hmemset<" # dtype ">");
+builder::dyn_var<uint8_t(uint8_t*,loop_type)> read_uint8_t = builder::as_global("hmda::read<uint8_t>");
+builder::dyn_var<uint16_t(uint16_t*,loop_type)> read_uint16_t = builder::as_global("hmda::read<uint16_t>");
+builder::dyn_var<uint32_t(uint32_t*,loop_type)> read_uint32_t = builder::as_global("hmda::read<uint32_t>");
+builder::dyn_var<uint64_t(uint64_t*,loop_type)> read_uint64_t = builder::as_global("hmda::read<uint64_t>");
+builder::dyn_var<int16_t(int16_t*,loop_type)> read_int16_t = builder::as_global("hmda::read<int16_t>");
+builder::dyn_var<int32_t(int32_t*,loop_type)> read_int32_t = builder::as_global("hmda::read<int32_t>");
+builder::dyn_var<int64_t(int64_t*,loop_type)> read_int64_t = builder::as_global("hmda::read<int64_t>");
+builder::dyn_var<float(float*,loop_type)> read_float = builder::as_global("hmda::read<float>");
+builder::dyn_var<double(double*,loop_type)> read_double = builder::as_global("hmda::read<double>");
 
-READER(uint8_t);
-READER(uint16_t);
-READER(uint32_t);
-READER(uint64_t);
-READER(int16_t);
-READER(int32_t);
-READER(int64_t);
-READER(float);
-READER(double);
+builder::dyn_var<void(uint8_t*,loop_type,uint8_t)> write_uint8_t = builder::as_global("hmda::write<uint8_t>");
+builder::dyn_var<void(uint16_t*,loop_type,uint16_t)> write_uint16_t = builder::as_global("hmda::write<uint16_t>");
+builder::dyn_var<void(uint32_t*,loop_type,uint32_t)> write_uint32_t = builder::as_global("hmda::write<uint32_t>");
+builder::dyn_var<void(uint64_t*,loop_type,uint64_t)> write_uint64_t = builder::as_global("hmda::write<uint64_t>");
+builder::dyn_var<void(int16_t*,loop_type,int16_t)> write_int16_t = builder::as_global("hmda::write<int16_t>");
+builder::dyn_var<void(int32_t*,loop_type,int32_t)> write_int32_t = builder::as_global("hmda::write<int32_t>");
+builder::dyn_var<void(int64_t*,loop_type,int64_t)> write_int64_t = builder::as_global("hmda::write<int64_t>");
+builder::dyn_var<void(float*,loop_type,float)> write_float = builder::as_global("hmda::write<float>");
+builder::dyn_var<void(double*,loop_type,double)> write_double = builder::as_global("hmda::write<double>");
 
-WRITER(uint8_t);
-WRITER(uint16_t);
-WRITER(uint32_t);
-WRITER(uint64_t);
-WRITER(int16_t);
-WRITER(int32_t);
-WRITER(int64_t);
-WRITER(float);
-WRITER(double);
+builder::dyn_var<uint8_t(HEAP_T<uint8_t>,loop_type)> read_heaparr_uint8_t = builder::as_global("hmda::read<uint8_t>");
+builder::dyn_var<uint16_t(HEAP_T<uint16_t>,loop_type)> read_heaparr_uint16_t = builder::as_global("hmda::read<uint16_t>");
+builder::dyn_var<uint32_t(HEAP_T<uint32_t>,loop_type)> read_heaparr_uint32_t = builder::as_global("hmda::read<uint32_t>");
+builder::dyn_var<uint64_t(HEAP_T<uint64_t>,loop_type)> read_heaparr_uint64_t = builder::as_global("hmda::read<uint64_t>");
+builder::dyn_var<int16_t(HEAP_T<int16_t>,loop_type)> read_heaparr_int16_t = builder::as_global("hmda::read<int16_t>");
+builder::dyn_var<int32_t(HEAP_T<int32_t>,loop_type)> read_heaparr_int32_t = builder::as_global("hmda::read<int32_t>");
+builder::dyn_var<int64_t(HEAP_T<int64_t>,loop_type)> read_heaparr_int64_t = builder::as_global("hmda::read<int64_t>");
+builder::dyn_var<float(HEAP_T<float>,loop_type)> read_heaparr_float = builder::as_global("hmda::read<float>");
+builder::dyn_var<double(HEAP_T<double>,loop_type)> read_heaparr_double = builder::as_global("hmda::read<double>");
 
-HREADER(uint8_t);
-HREADER(uint16_t);
-HREADER(uint32_t);
-HREADER(uint64_t);
-HREADER(int16_t);
-HREADER(int32_t);
-HREADER(int64_t);
-HREADER(float);
-HREADER(double);
+builder::dyn_var<void(HEAP_T<uint8_t>,loop_type,uint8_t)> write_heaparr_uint8_t = builder::as_global("hmda::write<uint8_t>");
+builder::dyn_var<void(HEAP_T<uint16_t>,loop_type,uint16_t)> write_heaparr_uint16_t = builder::as_global("hmda::write<uint16_t>");
+builder::dyn_var<void(HEAP_T<uint32_t>,loop_type,uint32_t)> write_heaparr_uint32_t = builder::as_global("hmda::write<uint32_t>");
+builder::dyn_var<void(HEAP_T<uint64_t>,loop_type,uint64_t)> write_heaparr_uint64_t = builder::as_global("hmda::write<uint64_t>");
+builder::dyn_var<void(HEAP_T<int16_t>,loop_type,int16_t)> write_heaparr_int16_t = builder::as_global("hmda::write<int16_t>");
+builder::dyn_var<void(HEAP_T<int32_t>,loop_type,int32_t)> write_heaparr_int32_t = builder::as_global("hmda::write<int32_t>");
+builder::dyn_var<void(HEAP_T<int64_t>,loop_type,int64_t)> write_heaparr_int64_t = builder::as_global("hmda::write<int64_t>");
+builder::dyn_var<void(HEAP_T<float>,loop_type,float)> write_heaparr_float = builder::as_global("hmda::write<float>");
+builder::dyn_var<void(HEAP_T<double>,loop_type,double)> write_heaparr_double = builder::as_global("hmda::write<double>");
 
-HWRITER(uint8_t);
-HWRITER(uint16_t);
-HWRITER(uint32_t);
-HWRITER(uint64_t);
-HWRITER(int16_t);
-HWRITER(int32_t);
-HWRITER(int64_t);
-HWRITER(float);
-HWRITER(double);
+builder::dyn_var<void(uint8_t*,uint8_t,loop_type)> memset_uint8_t = builder::as_global("hmda::hmemset<uint8_t>");
+builder::dyn_var<void(uint16_t*,uint16_t,loop_type)> memset_uint16_t = builder::as_global("hmda::hmemset<uint16_t>");
+builder::dyn_var<void(uint32_t*,uint32_t,loop_type)> memset_uint32_t = builder::as_global("hmda::hmemset<uint32_t>");
+builder::dyn_var<void(uint64_t*,uint64_t,loop_type)> memset_uint64_t = builder::as_global("hmda::hmemset<uint64_t>");
+builder::dyn_var<void(int16_t*,int16_t,loop_type)> memset_int16_t = builder::as_global("hmda::hmemset<int16_t>");
+builder::dyn_var<void(int32_t*,int32_t,loop_type)> memset_int32_t = builder::as_global("hmda::hmemset<int32_t>");
+builder::dyn_var<void(int64_t*,int64_t,loop_type)> memset_int64_t = builder::as_global("hmda::hmemset<int64_t>");
+builder::dyn_var<void(float*,float,loop_type)> memset_float = builder::as_global("hmda::hmemset<float>");
+builder::dyn_var<void(double*,double,loop_type)> memset_double = builder::as_global("hmda::hmemset<double>");
 
-MEMSET(uint8_t);
-MEMSET(uint16_t);
-MEMSET(uint32_t);
-MEMSET(uint64_t);
-MEMSET(int16_t);
-MEMSET(int32_t);
-MEMSET(int64_t);
-MEMSET(float);
-MEMSET(double);
+builder::dyn_var<void(HEAP_T<uint8_t>,uint8_t,loop_type)> memset_heaparr_uint8_t = builder::as_global("hmda::hmemset<uint8_t>");
+builder::dyn_var<void(HEAP_T<uint16_t>,uint16_t,loop_type)> memset_heaparr_uint16_t = builder::as_global("hmda::hmemset<uint16_t>");
+builder::dyn_var<void(HEAP_T<uint32_t>,uint32_t,loop_type)> memset_heaparr_uint32_t = builder::as_global("hmda::hmemset<uint32_t>");
+builder::dyn_var<void(HEAP_T<uint64_t>,uint64_t,loop_type)> memset_heaparr_uint64_t = builder::as_global("hmda::hmemset<uint64_t>");
+builder::dyn_var<void(HEAP_T<int16_t>,int16_t,loop_type)> memset_heaparr_int16_t = builder::as_global("hmda::hmemset<int16_t>");
+builder::dyn_var<void(HEAP_T<int32_t>,int32_t,loop_type)> memset_heaparr_int32_t = builder::as_global("hmda::hmemset<int32_t>");
+builder::dyn_var<void(HEAP_T<int64_t>,int64_t,loop_type)> memset_heaparr_int64_t = builder::as_global("hmda::hmemset<int64_t>");
+builder::dyn_var<void(HEAP_T<float>,float,loop_type)> memset_heaparr_float = builder::as_global("hmda::hmemset<float>");
+builder::dyn_var<void(HEAP_T<double>,double,loop_type)> memset_heaparr_double = builder::as_global("hmda::hmemset<double>");
 
-HMEMSET(uint8_t);
-HMEMSET(uint16_t);
-HMEMSET(uint32_t);
-HMEMSET(uint64_t);
-HMEMSET(int16_t);
-HMEMSET(int32_t);
-HMEMSET(int64_t);
-HMEMSET(float);
-HMEMSET(double);
-
-// Things for building heaparrs
-#define BUILDER(dtype)							\
-  builder::dyn_var<HEAP_T<dtype>(loop_type)> build_heaparr_##dtype = builder::as_global("hmda::build_heaparr<" # dtype ">");
-
-BUILDER(uint8_t);
-BUILDER(uint16_t);
-BUILDER(uint32_t);
-BUILDER(uint64_t);
-BUILDER(int16_t);
-BUILDER(int32_t);
-BUILDER(int64_t);
-BUILDER(float);
-BUILDER(double);
+builder::dyn_var<HEAP_T<uint8_t>(loop_type)> build_heaparr_uint8_t = builder::as_global("hmda::build_heaparr<uint8_t>");
+builder::dyn_var<HEAP_T<uint16_t>(loop_type)> build_heaparr_uint16_t = builder::as_global("hmda::build_heaparr<uint16_t>");
+builder::dyn_var<HEAP_T<uint32_t>(loop_type)> build_heaparr_uint32_t = builder::as_global("hmda::build_heaparr<uint32_t>");
+builder::dyn_var<HEAP_T<uint64_t>(loop_type)> build_heaparr_uint64_t = builder::as_global("hmda::build_heaparr<uint64_t>");
+builder::dyn_var<HEAP_T<int16_t>(loop_type)> build_heaparr_int16_t = builder::as_global("hmda::build_heaparr<int16_t>");
+builder::dyn_var<HEAP_T<int32_t>(loop_type)> build_heaparr_int32_t = builder::as_global("hmda::build_heaparr<int32_t>");
+builder::dyn_var<HEAP_T<int64_t>(loop_type)> build_heaparr_int64_t = builder::as_global("hmda::build_heaparr<int64_t>");
+builder::dyn_var<HEAP_T<float>(loop_type)> build_heaparr_float = builder::as_global("hmda::build_heaparr<float>");
+builder::dyn_var<HEAP_T<double>(loop_type)> build_heaparr_double = builder::as_global("hmda::build_heaparr<double>");
 
 }
