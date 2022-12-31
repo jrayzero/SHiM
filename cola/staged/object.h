5fe37ea54d733283d29791814357429b5dcffcc7
Jessica Ray <jray@csail.mit.edu>
2022-12-31 00:33:14 -0500

    do not use static for the default field name!

--- cola/staged/object.h
+++ cola/staged/object.h
@@ -83,6 +83,8 @@ struct StagedObject {
 
   // name of the particular instance
   std::string instance_name;
+  
+  int next_field = 0;
 
 private:
 
@@ -94,7 +96,8 @@ private:
 // Just a holder for some static things so don't need a template parameter
 // when accessing the static things in SField
 struct BareSField {
-  static inline const std::string repr = "sfield";
+  static inline const std::string repr_read = "sfield_read";
+  static inline const std::string repr_write = "sfield_write";
 };
 
 // If you don't care about the name (like, you're not gonna use this outside of the staged code), 
@@ -104,7 +107,7 @@ struct SField {
 
   // TODO only allow default values for fundamental types
   explicit SField(StagedObject *container, std::string name="", builder::dyn_var<Elem> def_val=0) : 
-    name(name.empty() ? "__field" + std::to_string(next_field++) : name), def_val(def_val),
+    name(name.empty() ? "__field" + std::to_string(container->next_field++) : name),
     object_name(container->name), instance_name(container->instance_name) {
     assert(!StagedObject::object_fields.empty());
     if (StagedObject::object_fields.top().count(this->name) != 0) {
@@ -112,19 +115,24 @@ struct SField {
       exit(48);
     }
     StagedObject::object_fields.top()[this->name] = elem_to_str<Elem>();
-    // TODO the def_val needs to use operator=  (once I implement it) so that I overwrite it appropriately
-    // and point to the struct
+    this->value = def_val;
+  }
+
+  void operator=(builder::dyn_var<Elem> rhs) {
+    builder::annotate(BareSField::repr_write + ":" + name + ":" + instance_name);
+    dummy_decl();
+    this->value = rhs;
   }
 
   operator builder::dyn_var<Elem>() {
-    builder::annotate(BareSField::repr + ":" + name + ":" + instance_name);
+    builder::annotate(BareSField::repr_read + ":" + name + ":" + instance_name);
     dummy_decl();
     return value;
   }
   
 private:
 
-  static inline int64_t next_field = 0;
+//  static inline int64_t next_field = 0;
 
   builder::dyn_var<Elem> value;
   // Name of this field
