86bbb5dec18e2e8c1be4761529304100f18a1e81
Jessica Ray <jray@csail.mit.edu>
2023-01-10 23:24:59 -0500

    add tests for user arrays with linear and nested data

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -16,21 +16,21 @@ static void staged() {
   auto block = Block<int,2>::stack<10,20>();
   auto view = block.view();
   auto view2 = block.view(slice(0,10,2),slice(3,19,4));
-  cola_assert(compare_arrays(block.bextents, darr{10, 20}), "compare_arrays(block.bextents, darr{10, 20})");
-  cola_assert(compare_arrays(block.bstrides, darr{1,1}), "compare_arrays(block.bstrides, darr{1,1})");
-  cola_assert(compare_arrays(block.borigin, darr{0,0}), "compare_arrays(block.borigin, darr{0,0})");
-  cola_assert(compare_arrays(view.bextents, darr{10,20}), "compare_arrays(view.bextents, darr{10,20})");
-  cola_assert(compare_arrays(view.bstrides, darr{1,1}), "compare_arrays(view.bstrides, darr{1,1})");
-  cola_assert(compare_arrays(view.borigin, darr{0,0}), "compare_arrays(view.borigin, darr{0,0})");
-  cola_assert(compare_arrays(view.vextents, darr{10,20}), "compare_arrays(view.vextents, darr{10,20})");
-  cola_assert(compare_arrays(view.vstrides, darr{1,1}), "compare_arrays(view.vstrides, darr{1,1})");
-  cola_assert(compare_arrays(view.vorigin, darr{0,0}), "compare_arrays(view.vorigin, darr{0,0})");
-  cola_assert(compare_arrays(view2.bextents, darr{10,20}), "compare_arrays(view2.bextents, darr{10,20})");
-  cola_assert(compare_arrays(view2.bstrides, darr{1,1}), "compare_arrays(view2.bstrides, darr{1,1})");
-  cola_assert(compare_arrays(view2.borigin, darr{0,0}), "compare_arrays(view2.borigin, darr{0,0})");
-  cola_assert(compare_arrays(view2.vextents, darr{5,4}), "compare_arrays(view2.vextents, darr{5,4})");
-  cola_assert(compare_arrays(view2.vstrides, darr{2,4}), "compare_arrays(view2.vstrides, darr{2,4})");
-  cola_assert(compare_arrays(view2.vorigin, darr{0,3}), "compare_arrays(view2.vorigin, darr{0,3})");
+  ASSERT(compare_arrays(block.bextents, darr{10, 20}));
+  ASSERT(compare_arrays(block.bstrides, darr{1,1}));
+  ASSERT(compare_arrays(block.borigin, darr{0,0}));
+  ASSERT(compare_arrays(view.bextents, darr{10,20}));
+  ASSERT(compare_arrays(view.bstrides, darr{1,1}));
+  ASSERT(compare_arrays(view.borigin, darr{0,0}));
+  ASSERT(compare_arrays(view.vextents, darr{10,20}));
+  ASSERT(compare_arrays(view.vstrides, darr{1,1}));
+  ASSERT(compare_arrays(view.vorigin, darr{0,0}));
+  ASSERT(compare_arrays(view2.bextents, darr{10,20}));
+  ASSERT(compare_arrays(view2.bstrides, darr{1,1}));
+  ASSERT(compare_arrays(view2.borigin, darr{0,0}));
+  ASSERT(compare_arrays(view2.vextents, darr{5,4}));
+  ASSERT(compare_arrays(view2.vstrides, darr{2,4}));
+  ASSERT(compare_arrays(view2.vorigin, darr{0,3}));
 }
 
 int main() {
---
+++ tests/test2_driver.cpp
@@ -0,0 +1,6 @@
+#include "test2_generated.h"
+
+int main() {
+  int arr[200];
+  staged(arr);
+}
